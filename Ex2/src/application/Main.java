f0eac2f0b55a1c6628f7fee9414e0bbe948a4f13
Ian Gurgel <iangurgel25@hotmail.com>
2025-09-01 18:29:56 -0300

    Feito: Ex2

---
+++ Ex2/src/application/Main.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int number = sc.nextInt();
+
+        if(number % 2 == 0){
+            System.out.println("O número " + number + " é par");
+        } else{
+            System.out.println("O número " + number + " é impar");
+        }
+
+        if(number > 0) {
+            System.out.println("O número " + number + " é positivo");
+        } else if(number == 0) {
+            System.out.println("O número " + number + " é igual a 0");
+        } else {
+            System.out.println("O número " + number + " é negativo");
+        }
+    }
+}
