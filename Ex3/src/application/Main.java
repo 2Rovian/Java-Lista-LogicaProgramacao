527651c0e638db617a61d86c90bd3adfdead7907
Ian Gurgel <iangurgel25@hotmail.com>
2025-09-01 19:09:30 -0300

    Corrigido: Ex3

--- Ex3/src/application/Main.java
+++ Ex3/src/application/Main.java
@@ -9,9 +9,14 @@ public class Main {
         int primeiroNumero = sc.nextInt();
         System.out.print("Digite o segundo número: ");
         int segundoNumero = sc.nextInt();
+        int c;
 
         if(primeiroNumero == segundoNumero) {
-            System.out.printf("A soma entre %d e %d é igual a %d", primeiroNumero, segundoNumero, primeiroNumero + segundoNumero);
+            c =  primeiroNumero + segundoNumero;
+            System.out.printf("A soma entre %d e %d é igual a %d", primeiroNumero, segundoNumero, c);
+        } else {
+            c = primeiroNumero * segundoNumero;
+            System.out.printf("A multiplicação entre %d e %d é igual a %d", primeiroNumero, segundoNumero, c);
         }
     }
 }
